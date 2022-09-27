2f47a64cacfc0bc22713239975987243b289c77d
José Manuel Nieto <superjmn@outlook.com>
2022-09-28 00:48:33 +0200

    Improved test

--- CodeGeneration.Tests/CodeGenerationTests.cs
+++ CodeGeneration.Tests/CodeGenerationTests.cs
@@ -1,26 +1,33 @@
 using CodeGeneration.Model;
 using Xunit.Abstractions;
 
-namespace CodeGeneration.Tests
+namespace CodeGeneration.Tests;
+
+public class CodeGenerationTests
 {
-    public class CodeGenerationTests
+    private readonly ITestOutputHelper output;
+
+    public CodeGenerationTests(ITestOutputHelper output)
     {
-        private readonly ITestOutputHelper output;
+        this.output = output;
+    }
 
-        public CodeGenerationTests(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
+    [Fact]
+    public void Simple_test()
+    {
+        var add = new BinaryExpression(new IdentifierExpression("b"), new IdentifierExpression("c"), '+');
+        var mult = new BinaryExpression(add, new IdentifierExpression("e"), '*');
+        var assignment = new AssignmentExpression(new IdentifierExpression("a"), mult);
 
-        [Fact]
-        public void Simple_test()
-        {
-            var add = new BinaryExpression(new IdentifierExpression("b"), new IdentifierExpression("c"), '+');
-            var mult = new BinaryExpression(add, new IdentifierExpression("e"), '*');
-            var assignment = new AssignmentExpression(new IdentifierExpression("a"), mult);
+        var instructions = new IntermediateCodeGenerator().Generate(assignment);
+        var format = string.Join(Environment.NewLine, instructions.Codes.ToCodeFormatContent());
+        var expected = @"T0 = b 
+                            T1 = c 
+                            T2 = T0 + T1
+                            T3 = e 
+                            T4 = T2 * T3
+                            a = T4 ";
 
-            var instructions = new IntermediateCodeGenerator().Generate(assignment);
-            instructions.Codes.ToCodeFormatContent().ToList().ForEach(s => output.WriteLine(s));
-        }
+        Assert.Equal(expected.RemoveWhitespace(), format.RemoveWhitespace());
     }
 }
---
+++ CodeGeneration.Tests/TestMixin.cs
@@ -0,0 +1,10 @@
+namespace CodeGeneration.Tests;
+
+public static class TestMixin
+{
+    public static string RemoveWhitespace(this string str)
+    {
+        var array = str.Where(c => !char.IsWhiteSpace(c)).ToArray();
+        return new string(array);
+    }
+}
